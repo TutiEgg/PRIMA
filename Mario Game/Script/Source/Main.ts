c7731c4b57667e3a4bff07b5d0b8150fd3ecf5bc
Luca <mmlpv.moser@gmx.de>
2022-10-17 20:26:28 +0200

    Sprite Update

--- Mario Game/Script/Source/Main.ts
+++ Mario Game/Script/Source/Main.ts
@@ -1,20 +1,31 @@
 namespace Script {
   import ƒ = FudgeCore;
+  //import ƒAid = FudgeAid;
+
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  let marioPos:ƒ.Node;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    console.log(viewport);
+    let branch: ƒ.Node = viewport.getBranch();
+    console.log(branch);
+    marioPos= branch.getChildrenByName("MarioPosition")[0]; // get Sprite by name
+    console.log("Mario",marioPos);
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+    console.log("Update");
+    // mtx = MAtrix 
+    marioPos.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.01);
   }
 }
